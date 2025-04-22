e3d5dbca9ea176073c0d2d9ce79d5c363b57d687
Walther Graciano <walther.graciano@gmail.com>
2025-04-22 14:18:51 -0300

    U

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import subprocess
-subprocess.run([
-            r"C:\Program Files\NAPS2\NAPS2.Console.exe",
-            '--output', "test.png",
-            '--noprofile',
-            '--driver', "twain",
-            '--device', "twain",
-            '--dpi', "600",
-        ], check=True, text=True)
+
+while True:
+    subprocess.run([
+        r"C:\Program Files\NAPS2\NAPS2.Console.exe",
+        '--output', "test.png",
+        '--noprofile',
+        '--driver', "twain",
+        '--device', "twain",
+        '--dpi', "600",
+    ], check=True, text=True)
+
+    input("Press Enter to scan the next page...")
